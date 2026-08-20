fbce54374ff7ed70d2f4f1605d5f2c8f51e01a5f
Saeed Ya <LightMoon@users.noreply.github.com>
2026-08-20 12:03:36 +1000

    Bump Issue #3 release metadata to 0.1.3

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_autobrowsertimezone';
-$plugin->version = 2026082002;
-$plugin->release = '0.1.2';
+$plugin->version = 2026082003;
+$plugin->release = '0.1.3';
 $plugin->requires = 2024100700; // Moodle 4.5.
 $plugin->supported = [405, 502];
 $plugin->maturity = MATURITY_ALPHA;
